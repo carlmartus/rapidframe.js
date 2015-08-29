d04fdca1c433e3d1f2232b8ae42a4588d9b8cf7d
Martin Sandgren <carlmartus@gmail.com>
2015-08-29 22:44:30 +0200

    Mouse capture

--- README.md
+++ README.md
@@ -5,16 +5,16 @@ Fast game development using WebGL.
 ## Current state
  * [X] WebGL frame initialization
  * [ ] Full frame
- * [ ] Full screen (LD compatible)
- * [ ] Mouse capture
+ * [X] Full screen (LD compatible)
+ * [X] Mouse capture
  * [ ] Resource downloading screen
  * [X] GLSL Shader loading
  * [ ] Texture loading
  * [X] Vectors (2D-4D)
  * [X] Matrices (4x4)
  * [X] Shared shader uniforms constants
- * [ ] Keyboard events
- * [ ] Mouse events
+ * [X] Keyboard events
+ * [X] Mouse events
  * [ ] 2D sprite
  * [ ] 2D font
 
--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 function rfGame(tagId) {
+	var self = this;
+
 	this.tagId = tagId;
+	this.tag = document.getElementById(this.tagId);
+	this.clientW = this.tag.clientWidth;
+	this.clientH = this.tag.clientHeight;
 	this.gl = null;
 	this.gl_arrayCount = 0;
+	this.mouseAutoCapture = false;
+
 	this.cbResize = null;
+	this.cbMouseMove = null;
+	this.cbMouseClick = null;
 
 	this.blockRender = false;
 	this.enabledFullscreen = false;
 	this.enabledKeyboard = false;
-	this.keyBinds = {};
+	this.enabledMouse = false;
+	this.enabledLogKeys = false;
 
-	var self = this;
+	this.keyBinds = {};
 
 	window.addEventListener('blur', function() {
 		self.blockRender = true;
@@ -17,19 +27,37 @@ function rfGame(tagId) {
 	window.addEventListener('focus', function() {
 		self.blockRender = false;
 	});
+
+	// Resize event
+	this.tag.addEventListener('resize', function() {
+		self._resize();
+	}, false);
+
+	// Fullscreen events
+	if (this.tag.requestFullscreen) {
+		document.addEventListener('fullscreenchange', function() {
+			self._eventFullScreen(document.fullScreenEnabled);
+		}, false);
+	} else if (this.tag.mozRequestFullScreen) {
+		document.addEventListener('mozfullscreenchange', function() {
+			self._eventFullScreen(document.mozFullScreenElement != null);
+		}, false);
+	} else if (this.tag.webkitRequestFullscreen) {
+		document.addEventListener('webkitfullscreenchange', function() {
+			self._eventFullScreen(document.webkitFullscreenElement != null);
+		}, false);
+	}
 }
 
 rfGame.prototype.setupWebGl = function(webGlOptions) {
-	var tag = document.getElementById(this.tagId);
-
 	this.gl = null;
 	try {
-		this.gl = tag.getContext('webgl', webGlOptions);
+		this.gl = this.tag.getContext('webgl', webGlOptions);
 	} catch (e) {}
 
 	if (this.gl == null) {
 		try {
-			this.gl = tag.getContext('experimental-webgl', webGlOptions);
+			this.gl = this.tag.getContext('experimental-webgl', webGlOptions);
 		} catch (e) {
 			alert('Could not initialize WebGL');
 		}
@@ -63,30 +91,53 @@ rfGame.prototype.startLoop = function(frame, render) {
 	requestAnimationFrame(clo);
 };
 
+rfGame.prototype.autoCaptureMouse = function() {
+	var el = this.tag;
+
+	if (el.requestPointerLock) {
+		el.requestPointerLock();
+	} else if (el.mozRequestPointerLock) {
+		el.mozRequestPointerLock();
+	} else if (el.webkitRequestPointerLock) {
+		el.webkitRequestPointerLock();
+	}
+};
+
+rfGame.prototype.setMouseCallback = function(cbMove, cbClick) {
+	this.cbMouseMove = cbMove;
+	this.cbMouseClick = cbClick;
+
+	if (!this.enabledMouse) {
+		this._listenMouse();
+	}
+};
+
 rfGame.prototype.isFullscreen = function(on) {
 	return this.enabledFullscreen;
 };
 
 rfGame.prototype.setFullscreen = function(on) {
-};
-
-rfGame.prototype.setMouseMoveCallback = function(cb) {
-};
-
-rfGame.prototype._keyEvent = function(event, press) {
-	console.log('Key', event.keyCode, press);
-	var bind = this.keyBinds[event.keyCode];
-	if (bind) bind(event.keyCode, press, event);
+	if (on && !this.enabledFullscreen) {
+		_goFullscreen(this.tag);
+	} else if (this.enabledFullscreen) {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitExitFullscreen) {
+			document.webkitExitFullscreen();
+		}
+	}
 };
 
 rfGame.prototype.bindKey = function(key, cb) {
 	var self = this;
 	if (!this.enabledKeyboard) {
 		document.addEventListener('keydown', function(event) {
-			self._keyEvent(event.keyCode, true);
+			self._keyEvent(event, true);
 		});
 		document.addEventListener('keyup', function(event) {
-			self._keyEvent(event.keyCode, false);
+			self._keyEvent(event, false);
 		});
 		this.enabledKeyboard = true;
 	}
@@ -94,9 +145,19 @@ rfGame.prototype.bindKey = function(key, cb) {
 	this.keyBinds[key] = cb;
 };
 
+rfGame.prototype.setLogKeys = function(on) {
+	this.enabledLogKeys = on;
+};
+
 rfGame.prototype.setResizeCallback = function(cb) {
 	this.cbResize = cb;
 };
+
+rfGame.prototype.setMouseMoveCallback = function(cb) {
+};
+
+// GL Misc help
+// ============
 
 rfGame.prototype.glArrayCount = function(count) {
 	while (this.gl_arrayCount > count) {
@@ -107,4 +168,96 @@ rfGame.prototype.glArrayCount = function(count) {
 		this.gl.enableVertexAttribArray(this.gl_arrayCount++);
 	}
 };
+
+// Internal
+// ========
+
+rfGame.prototype._resize = function() {
+	this.cbResize(this.tag.width, this.tag.height);
+};
+
+rfGame.prototype._keyEvent = function(event, press) {
+	if (event.repeat) return;
+
+	if (press && this.enabledLogKeys) {
+		console.log(event, 'Press:', press);
+	}
+
+	var bind = this.keyBinds[event.keyCode];
+	if (bind) bind(event.keyCode, press, event);
+};
+
+rfGame.prototype._eventFullScreen = function(set) {
+	this.enabledFullscreen = set;
+
+	if (set) {
+		this.tag.width = screen.width;
+		this.tag.height = screen.height;
+		this._resize();
+	} else {
+		this.tag.width = this.clientW;
+		this.tag.height = this.clientH;
+		this._resize();
+	}
+};
+
+rfGame.prototype._eventMouseLockChange = function(event) {
+	var can = this.tag;
+	if (
+			document.pointerLockElement === can ||
+			document.mozPointerLockElement === can ||
+			document.webkitPointerLockElement === can) {
+		this.mouseAutoCapture = true;
+		console.log(true);
+	} else {
+		this.mouseAutoCapture = false;
+		console.log(false);
+	}
+};
+
+rfGame.prototype._listenMouse = function() {
+	var self = this;
+
+	document.addEventListener('pointerlockchange', this._eventMouseLockChange, false);
+	document.addEventListener('mozpointerlockchange', this._eventMouseLockChange, false);
+	document.addEventListener('webkitpointerlockchange', this._eventMouseLockChange, false);
+
+	this.tag.addEventListener('mousemove', function(event) {
+		var x = event.clientX - self.tag.offsetLeft;
+		var y = event.clientY - self.tag.offsetTop;
+
+		var dx =
+			event.movementX ||
+			event.mozMovementX ||
+			event.webkitMovementX ||
+			0;
+		var dy =
+			event.movementY ||
+			event.mozMovementY ||
+			event.webkitMovementY ||
+			0;
+
+		if (self.cbMouseMove)	self.cbMouseMove(x, y, dx, dy);
+	}, false);
+
+	this.tag.addEventListener('mousedown', function(event) {
+		if (self.cbMouseClick)	self.cbMouseClick(true);
+	}, false);
+
+	this.tag.addEventListener('mouseup', function(event) {
+		if (self.cbMouseClick)	self.cbMouseClick(false);
+	}, false);
+}
+
+function _goFullscreen(el) {
+	if (el.requestFullscreen) {
+		el.requestFullscreen();
+	} else if (el.mozRequestFullScreen) {
+		el.mozRequestFullScreen();
+	} else if (el.webkitRequestFullscreen) {
+		el.webkitRequestFullscreen();
+	} else if (el.msRequestFullscreen) {
+		el.msRequestFullscreen();
+	}
+}
 
